96f6ff5a6b4c3769f940e09c1c42a35885cebbd8
Carson, Nessa <nessa.carson@astrazeneca.com>
2023-07-02 11:46:02 +0100

    Allow forced use of no proxy

--- Compound_lookup/add_pKa.py
+++ Compound_lookup/add_pKa.py
@@ -73,3 +73,4 @@ headers = {
     'user-agent': 'Mozilla/5.0 (X11; CentOS; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
     'Connection': 'close'}
 proxy_file = Path('proxies.dat')
+proxies_none = {'http': '', 'https': ''} # To force use of no proxy
